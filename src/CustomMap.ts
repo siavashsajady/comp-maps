b812009e2f4779f2e18fd681d5bedb8229f41787
siavashsajady <hubgitpro@gmail.com>
2022-08-13 10:34:51 +0200

    Add popup feature

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,11 @@
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
+
+  color: string;
 }
 
 export class CustomMap {
@@ -19,12 +22,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
